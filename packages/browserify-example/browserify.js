a75eb668e7bde83b44115bdd01e3edfb172a2d34
Eli Doran <eli@elidoran.com>
2015-05-26 20:09:36 -0400

    add process.env use to show envify transform

--- packages/browserify-example/browserify.js
+++ packages/browserify-example/browserify.js
@@ -1,7 +1,13 @@
 console.log("example1 browserify.js");
 
-// w/out var it becomes a package scoped variable 
+// w/out var it becomes a package scoped variable
 uppercase1 = require('upper-case');  // exports a function
 
 // we'll make this one global scoped via api.export() in package.js
 appUppercase = uppercase1;
+
+
+if (process.env.NODE_ENV === 'production')
+  console.log('it\'s production!!');
+else
+  console.log('it\'s development...');
