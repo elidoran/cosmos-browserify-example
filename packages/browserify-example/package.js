fc82b244d3374e88e55597e5105ea47ff888c91f
Eli Doran <eli@elidoran.com>
2015-06-21 19:57:27 -0400

    + cosmos:browserify 0.4.0

--- packages/browserify-example/package.js
+++ packages/browserify-example/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cosmos:browserify-example',
-  version: '0.2.0',
+  version: '0.2.1',
   summary: 'First example package using cosmos:browserify',
   git: '',
   documentation: 'README.md'
@@ -15,12 +15,13 @@ Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
   api.use(['coffeescript', 'templating'], 'client');
 
-  api.use(['cosmos:browserify@0.3.0'], 'client'); // need this package
+  api.use(['cosmos:browserify@0.4.0'], 'client'); // need this package
 
   api.addFiles([
       'client/example.html',    // show some example results
       'client/example.coffee',  // package's Meteor script
-      'client.browserify.js'           // browserify file
+      'client.browserify.js',           // browserify file
+      'client.browserify.options.json'  // browserify options file
     ],
     'client'
   );
